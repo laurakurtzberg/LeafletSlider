4076c4854b532901273434e4f3a2821865ec51a2
Laura Kurtzberg <laura.kurtzberg@gmail.com>
2015-05-19 09:23:28 -0700

    Update SliderControl.js
    width again

--- SliderControl.js
+++ SliderControl.js
@@ -39,7 +39,7 @@ L.Control.SliderControl = L.Control.extend({
 
         // Create a control sliderContainer with a jquery ui slider
         var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
-        $(sliderContainer).append('<div id="leaflet-slider" style="width:300px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; margin-top:13px; background-color:#FFFFFF; text-align:center; border-radius:5px;">arraste o botão para avançar o tempo</div></div>');
+        $(sliderContainer).append('<div id="leaflet-slider" style="width:240px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; margin-top:13px; background-color:#FFFFFF; text-align:center; border-radius:5px;">arraste o botão para avançar o tempo</div></div>');
         //Prevent map panning/zooming while using the slider
         $(sliderContainer).mousedown(function () {
             map.dragging.disable();
